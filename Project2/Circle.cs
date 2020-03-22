dc19fea15d8cb2d1332efd159deab57a5c4e9dc8
polinalepehina@gmail.com <polinalepehina@gmail.com>
2020-03-23 00:43:43 +0200

    Добавьте файлы проекта.

---
+++ Project2/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Circle
+    {
+        
+        private double radius;
+        public double Radius { get => radius; set => radius = value; }
+        const double PI = 3.14;
+
+        public double getSquare()
+        {
+            return PI* Radius * Radius;
+        }
+
+        public double getCircumference()
+        {
+            return 2 * PI * Radius;
+        }
+
+        public void printCircle()
+        {
+            Console.Write($"Circle: radius = {Radius}, square = {String.Format("{0:0.00}", getSquare())},  perimeter = {String.Format("{0:0.00}", getCircumference())}");
+        }
+
+    }
+}
+
+       
---
+++ Project2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Circle circle = new Circle();
+            Console.WriteLine("Input Radius: ");
+            double Radius;
+            circle.Radius = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine($"Square = {circle.getSquare()}");
+            Console.WriteLine($"Circumference = {circle.getCircumference()}");
+            Console.ReadKey();
+
+        }
+    }
+}
+   
+
